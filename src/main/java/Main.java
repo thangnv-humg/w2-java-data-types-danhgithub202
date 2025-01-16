c3368eac834acd510d769bae8023c84e0594c429
“danhgithub202 <daosongdanh2003tb@gmail.com>
2025-01-16 13:57:38 +0700

    bai thuc hanh so1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        // TODO: Khai báo biến và viết chương trình theo yêu cầu trong README.md
+        // Bước 1: Khai báo và gán giá trị cho các biến
+        int myInt = 42;
+        double myDouble = 3.14;
+        String myString = "Hello, Java!";
 
-        // Ví dụ:
-        // int myInt = 42;
-        // double myDouble = 3.14;
-        // String myString = "Hello, Java!";
-        // System.out.println(myInt);
-        // System.out.println(myDouble);
-        // System.out.println(myString);
+        // In các giá trị của ba biến ra màn hình
+        System.out.println("Giá trị của myInt: " + myInt);
+        System.out.println("Giá trị của myDouble: " + myDouble);
+        System.out.println("Giá trị của myString: " + myString);
+
+        // Bước 2: Kiểm tra giá trị của biến myInt
+        if (myInt > 10) {
+            System.out.println(" Lớn hơnhơn 10");
+        } else {
+            System.out.println("Nhỏ hơn hoặc bằng 10");
+        }
     }
 }
